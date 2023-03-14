69f04c627dd0148ce93a8dddfe1c5374e6a43c06
Yeferzon David Quisoboni <daavid@misena.edu.co>
2023-03-14 18:40:02 -0500

    Se crea view index, se retorna el template cities

--- citiesapp/views.py
+++ citiesapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import City
+
+#import libreria de todos los paises
 
 # Create your views here.
+def index(request):
+    cities = City.objects.all()
+
+    return render(request, 'cities.html', {'cities':cities})
