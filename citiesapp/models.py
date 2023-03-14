45327f14dcff4d612976283d684060d35a99f985
Yeferzon David Quisoboni <daavid@misena.edu.co>
2023-03-14 18:34:50 -0500

    Se crea modelo City en donde se agrega la clase Meta y se agrega función str para retornar nombre del objeto

--- citiesapp/models.py
+++ citiesapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+    image = models.ImageField(upload_to='citiesapp/photos')
+    population = models.IntegerField()
+    country = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Cities'
