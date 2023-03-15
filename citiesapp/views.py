ca30784f64a08a89eb2e21150ec73b67886380b5
Yeferzon David Quisoboni <daavid@misena.edu.co>
2023-03-14 19:57:25 -0500

    Se agrega función get_city  que retornará la inforación de una ciudad especifica

--- citiesapp/views.py
+++ citiesapp/views.py
@@ -9,3 +9,9 @@ def index(request):
     cities = City.objects.all()
 
     return render(request, 'cities.html', {'cities':cities})
+
+
+def get_city(request, id):
+    city = City.objects.get(id=id)
+
+    return render(request, 'city.html', {'city':city})
